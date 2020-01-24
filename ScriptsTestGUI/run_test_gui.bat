ef23eaaaaec2a5d3c67ca02b391f158c983be359
Christian Bouchard <christian.bouchard@cidco.ca>
2020-01-24 10:47:10 -0500

    Test: changed 'Release' to 'release'

--- ScriptsTestGUI/run_test_gui.bat
+++ ScriptsTestGUI/run_test_gui.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cd buildTest\Release\folderRunTest
+cd buildTest\release\folderRunTest
 
 testGUI.exe -o test-report-OpenSidescanXUNIT.xml,xunitxml -o test-report-OpenSidescan.xml,xml -o test-report-OpenSidescanTAP.txt,tap -o test-report-OpenSidescanTXT.txt,txt
 

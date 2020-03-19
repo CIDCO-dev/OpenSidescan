796b8f5465adc0080927ebbfb270689ef077a8d0
Christian Bouchard <christian.bouchard@cidco.ca>
2020-03-19 15:53:13 -0400

    Unit test: testing 5 objects but positions are in the code

--- test/unitTests/GeoreferencingTest.cpp
+++ test/unitTests/GeoreferencingTest.cpp
@@ -1,6 +1,9 @@
 
 #include <iostream>
 
+#include <vector>
+#include <map>
+#include <utility> // std::pair, std::make_pair
 
 #include "catch.hpp"
 
@@ -11,65 +14,36 @@
 
 #include "../../src/thirdParty/MBES-lib/src/math/Distance.hpp"
 
+typedef std::map< std::string, std::pair< double, double >, std::less< std::string > > MapObjectNameLongLat;
+
+
 TEST_CASE( "Test Caris vs. OpenSidescan" ) {
 
+    std::cout << std::fixed << std::setprecision( 15 );
 
-    std::cout << "\nBeginning of TEST_CASE\n" << std::endl;
 
+    const double maxAcceptableDistance = 5.0;
 
-//    int a = 1;
-
-//    REQUIRE( a == 1);
-
-//    REQUIRE( a == 2 );
-
-    // The Scotsman wreck is on channel 0: Port
+    // The Scotsman wreck is on channel 0: "Port"
     std::string sFileName = "../data/wrecks/scotsman5.xtf";
     int channelIndex = 0;
 
 
+    // TODO: Read data position of Caris from csv file
 
-//    W
-//    x1: 370
-//    y1: 654
-//    x2: 380
-//    y2: 661
-//    width: 10
-//    height: 7
+    //    V, -68.828257, 48.445629,
+    //    W, -68.828268, 48.445548,
+    //    X, -68.828327, 48.445632,
+    //    Y, -68.828370, 48.445703,
+    //    Z, -68.828345, 48.445666,
 
-//    V
-//    x1: 461
-//    y1: 722
-//    x2: 471
-//    y2: 730
-//    width: 10
-//    height: 8
+    MapObjectNameLongLat carisData;
 
-//    X
-//    x1: 388
-//    y1: 752
-//    x2: 397
-//    y2: 764
-//    width: 9
-//    height: 12
-
-//    Z
-//    x1: 399
-//    y1: 805
-//    x2: 412
-//    y2: 816
-//    width: 13
-//    height: 11
-
-//    Y
-//    x1: 382
-//    y1: 854
-//    x2: 394
-//    y2: 865
-//    width: 12
-//    height: 11
-
-
+    carisData.insert( std::make_pair( "V", std::make_pair( -68.828257, 48.445629 ) ) );
+    carisData.insert( std::make_pair( "W", std::make_pair( -68.828268, 48.445548 ) ) );
+    carisData.insert( std::make_pair( "X", std::make_pair( -68.828327, 48.445632 ) ) );
+    carisData.insert( std::make_pair( "Y", std::make_pair( -68.828370, 48.445703 ) ) );
+    carisData.insert( std::make_pair( "Z", std::make_pair( -68.828345, 48.445666 ) ) );
 
 
     DatagramParser * parser = nullptr;
@@ -114,54 +88,108 @@ TEST_CASE( "Test Caris vs. OpenSidescan" ) {
     std::cout << "\nChannel name: " << image->getChannelName() << "\n" << std::endl;
 
 
+    // TODO: Read OpenSidescan pixels information from a file
+
+    std::vector< std::string > objectNamesOpenSidescan;
+
+    std::vector< std::vector<int> > pixelInfo;
+
     //    W
-    int x1 = 370;
-    int y1 = 654;
-    //    x2: 380
-    //    y2: 661
-    int width = 10;
-    int height = 7;
+    //    x1: 370
+    //    y1: 654
+    //    width: 10
+    //    height: 7
+
+    //    V
+    //    x1: 461
+    //    y1: 722
+    //    width: 10
+    //    height: 8
+
+    //    X
+    //    x1: 388
+    //    y1: 752
+    //    width: 9
+    //    height: 12
+
+    //    Z
+    //    x1: 399
+    //    y1: 805
+    //    width: 13
+    //    height: 11
+
+    //    Y
+    //    x1: 382
+    //    y1: 854
+    //    width: 12
+    //    height: 11
+
+    objectNamesOpenSidescan.push_back( "W" );
+    pixelInfo.push_back( std::vector<int>( { 370, 654, 10, 7 } ) ); // x1, y1, width, height
+
+    objectNamesOpenSidescan.push_back( "V" );
+    pixelInfo.push_back( std::vector<int>( { 461, 722, 10, 8 } ) ); // x1, y1, width, height
+
+    objectNamesOpenSidescan.push_back( "X" );
+    pixelInfo.push_back( std::vector<int>( { 388, 752, 9, 12 } ) ); // x1, y1, width, height
+
+    objectNamesOpenSidescan.push_back( "Z" );
+    pixelInfo.push_back( std::vector<int>( { 399, 805, 13, 11 } ) ); // x1, y1, width, height
+
+    objectNamesOpenSidescan.push_back( "Y" );
+    pixelInfo.push_back( std::vector<int>( { 382, 854, 12, 11 } ) ); // x1, y1, width, height
 
 
-    GeoreferencedObject objectW ( *file,
-                                  *image,
-                                  x1, y1,
-                                  width, height );
+    for (int count = 0; count < objectNamesOpenSidescan.size(); count++ )
+    {
+        GeoreferencedObject object ( *file,
+                                      *image,
+                                      pixelInfo[ count ][ 0 ], pixelInfo[ count ][ 1 ],
+                                      pixelInfo[ count ][ 2 ], pixelInfo[ count ][ 3 ] );
 
-    Position * position = objectW.getPosition();
+        Position * position = object.getPosition();
 
-    std::cout << std::fixed << std::setprecision( 15 )
-              << "\nOpenSidescan:"
-              << "\nW longitude: " << position->getLongitude()
-              << "\nW latitude:  " << position->getLatitude() << "\n" << std::endl;
+        std::cout << "\n" << count << ") Object name: " << objectNamesOpenSidescan[ count ] << std::endl;
 
-
-
-
-
-    // Caris W, -68.828268, 48.445548
-
-    double carisWlongitude = -68.828268;
-    double carisWlatitude = 48.445548;
-
-    std::cout << std::fixed << std::setprecision( 15 )
-              << "\nCaris:"
-              << "\nW longitude: " << carisWlongitude
-              << "\nW latitude:  " << carisWlatitude << "\n" << std::endl;
+        std::cout << std::fixed << std::setprecision( 15 )
+                  << "\nOpenSidescan:"
+                  << "\n  longitude: " << position->getLongitude()
+                  << "\n  latitude:  " << position->getLatitude() << "\n" << std::endl;
 
 
+        std::string objectNameToFind = objectNamesOpenSidescan[ count ];
+        MapObjectNameLongLat::const_iterator mapLocation = carisData.find( objectNameToFind );
 
-    std::cout << "\nDistance: "
-              << Distance::haversine( carisWlongitude, carisWlatitude,
-                                      position->getLongitude(), position->getLatitude() )
-              << "\n" << std::endl;
 
+        REQUIRE( mapLocation != carisData.end() );
+
+        if ( mapLocation != carisData.end() ) {
+
+//            std::cout << "\n\"" << objectNameToFind << "\" found\n" << mapLocation->second.first << "\n"
+//                      << mapLocation->second.second << "\n" << std::endl;
+
+            std::cout << std::fixed << std::setprecision( 15 )
+                      << "\nCaris:"
+                      << "\n  longitude: " << mapLocation->second.first
+                      << "\n  latitude:  " << mapLocation->second.second << "\n" << std::endl;
+
+            double distance = Distance::haversine( mapLocation->second.first, mapLocation->second.second,
+                                                   position->getLongitude(), position->getLatitude() );
+
+            std::cout << "\nDistance: " << distance << "\n" << std::endl;
+
+            REQUIRE( distance <= maxAcceptableDistance );
+        }
+//        else {
+//            std::cout << "\n\"" << objectNameToFind << "\" not found\n"<< std::endl;
+//        }
+
+
+    }
 
 
     if ( file != nullptr )
         delete file;
-
-
 }
 
 

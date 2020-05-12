0caceba7bca212e363455c80bd282b752331717e
glm <admin@omnibustechnologies.ca>
2020-05-12 02:09:54 -0400

    Refactor thread-safe

---
+++ src/OpenSidescan/ui/mainwindow/monitorthread.cpp
@@ -0,0 +1,6 @@
+#include "monitorthread.h"
+
+MonitorThread::MonitorThread()
+{
+
+}
---
+++ src/OpenSidescan/ui/mainwindow/monitorthread.h
@@ -0,0 +1,11 @@
+#ifndef MONITORTHREAD_H
+#define MONITORTHREAD_H
+
+
+class MonitorThread
+{
+public:
+    MonitorThread();
+};
+
+#endif // MONITORTHREAD_H

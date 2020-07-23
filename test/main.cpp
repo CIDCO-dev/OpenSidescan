469ac3e53d2a3c0219f29e9d2ac808edc8673623
jordan <jordan@OpenSideScanJordanVM.cidco.ca>
2020-07-23 09:46:10 -0400

    removed dependency test

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "DependencyTest.hpp"
-/*
+//#include "DependencyTest.hpp"
 #include "SideScanImagerTest.hpp"
 #include "GeoreferencingTest.hpp"
 #include "DetectObjectsTest.hpp"
- */
 

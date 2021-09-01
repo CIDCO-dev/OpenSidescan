dabcac926642063948c47945d8d66725ed3a0c0c
patrick <patrick.cmorneau@cidco.ca>
2021-09-01 09:56:31 -0400

    fix script

--- Scripts/build_linux_unit_tests.sh
+++ Scripts/build_linux_unit_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p ../test/build
-cd ../test/build
+mkdir -p test/build
+cd test/build
 cmake ..
 make
-# a la version 3.21 de cmake on peut ajouter le parametre --output-junit pour que le reultat des tests soit en format jenkins
+# a la version 3.21 de cmake on peut ajouter le parametre --output-junit pour que le resultat des tests soit en format jenkins
 # pour le moment on va pas executer les tests 2 fois ;)
 # ctest --output-junit

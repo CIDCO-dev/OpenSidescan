d0be6fc497c4470a8f75643999d819caf94cfdfd
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-07 10:16:00 -0700

    not a win fan

--- Scripts/winlocktest.bat
+++ Scripts/winlocktest.bat
@@ -1,4 +1,5 @@
 mkdir test\win-fileLock-test\build
 cd test/win-fileLock-test/build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
-call "MSBuild.exe" wintest.sln
+MSBuild.exe wintest.sln
+test\\win-fileLock-test\\build\\Debug\\wincatchLockTest.exe -r junit -o build\\reports\\winlock-test-report.xml

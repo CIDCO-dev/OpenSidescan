0bca60cf50e98a32d2f4461f03b2f5a47295619d
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-07 11:22:12 -0700

    dont stop the dancin

--- Scripts/winlocktest.bat
+++ Scripts/winlocktest.bat
@@ -3,4 +3,4 @@ mkdir build\reports\
 cd test/win-fileLock-test/build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
 MSBuild.exe wintest.sln
-Debug\\wincatchLockTest.exe -r junit -o build\\reports\\winlock-test-report.xml
+Debug\\wincatchLockTest.exe -r junit -o ..\\..\\..\\build\\reports\\winlock-test-report.xml

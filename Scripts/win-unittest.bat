476b79632a1c03b59eb7223e19edf7c865419189
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-08 13:32:24 -0700

    pas ma meilleur journee

--- Scripts/win-unittest.bat
+++ Scripts/win-unittest.bat
@@ -3,5 +3,5 @@ mkdir build\reports\
 cd test/build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
 MSBuild.exe tests.sln
-Move Debug\* ../
+Move Debug\* ../build
 tests.exe -r junit -o ../../build/reports/win-unittest-result.xml

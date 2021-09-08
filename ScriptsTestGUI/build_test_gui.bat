025575a0932c7c6d16f9327ce1ab223538c187b3
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-08 07:25:13 -0700

    gui test

--- ScriptsTestGUI/build_test_gui.bat
+++ ScriptsTestGUI/build_test_gui.bat
@@ -2,5 +2,5 @@ mkdir test\testGUI\build\
 mkdir build\reports\
 cd test/testGUI/build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
-MSBuild.exe Opensidescan_gui_Tests.exe
+MSBuild.exe Opensidescan_gui_Tests.sln
 Debug\\wincatchLockTest.exe -r junit -o ..\\..\\..\\build\\reports\\testGUI_result.xml

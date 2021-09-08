0aefb1876a465a9ab01c3c7123c99f08e7dd2ea0
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-08 13:20:44 -0700

    building unit test 4 windows

--- Scripts/win-unittest.bat
+++ Scripts/win-unittest.bat
@@ -2,6 +2,6 @@ mkdir test\build\
 mkdir build\reports\
 cd test/build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
-MSBuild.exe Debug\win_unit_test.sln
+MSBuild.exe Debug\tests.sln
 Move Debug\* ../
 tests.exe -r junit -o ../../build/reports/win-unittest-result.xml

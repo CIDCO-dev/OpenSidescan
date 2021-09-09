e8058f8f0e1d5d963dd803764be04313bf7a236d
patrick charron morneau <patrick.cmorneau@cidco.ca>
2021-09-09 10:53:04 -0700

    locketest fixed again

--- test/win-fileLock-test/winFileLockTest.hpp
+++ test/win-fileLock-test/winFileLockTest.hpp
@@ -57,7 +57,7 @@ TEST_CASE("Test file lock with monitor, windows version") {
     si.cb = sizeof (si);
     ZeroMemory(&pi, sizeof (pi));
 	
-    if (CreateProcess("winlocker.exe",
+    if (CreateProcess("test\\win-fileLock-test\\build\\Debug\\winlocker.exe",
             " test\\data\\lockTest\\s4.xtf", // Additional application arguments
             NULL,
             NULL,

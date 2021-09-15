aae78235d3c59e6d25116592decb78a4e7e63d33
patrick <patrick.cmorneau@cidco.ca>
2021-09-15 10:02:18 -0400

    jenkins said perm is not ok but yes they are

--- ScriptsTestGUI/gui_test_linux.sh
+++ ScriptsTestGUI/gui_test_linux.sh
@@ -4,4 +4,6 @@ mkdir -p test/testGUI/build
 cd test/testGUI/build
 cmake ..
 make
-
+cd ../../../
+mkdir -p build/reports
+./test/testGUI/build/Opensidescan_gui_Tests -o build/reports/linux-testGUI.xml -xunitxml
